903bc8850148a46da29bc8c556b66da14ec08be5
Bob Terminator <igor_zhurbenko@mail.ru>
2020-11-07 14:00:29 +0700

    Добавьте файлы проекта.

---
+++ PureCLib/pch.h
@@ -0,0 +1,10 @@
+﻿
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+
+extern "C" MATHLIBRARY_API int factorial(
+    int a);
